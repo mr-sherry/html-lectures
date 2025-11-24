8416c4fab99aceedf5fa18bbe431ddb6fea8cd6c
mr.sherry <mr.sherry6967@gmail.com>
2025-11-24 16:28:56 +0500

    js start

---
+++ js/lec1/index.js
@@ -0,0 +1,22 @@
+// let var const
+
+// let hello = 411;
+// var number = 7777;
+// let number = 9023;
+const number = 2908932;
+
+let string = "hfkdsf83u32/'';;";
+let hello = undefined;
+let value = null;
+let boolean = false;
+
+let obj = { name: 'ali', age: 53 };
+let stdNames = ['ali', 'ahmed', 'raza'];
+
+console.log(typeof obj);
+console.log(typeof value);
+
+// function {
+//     number = 898;
+
+// }
