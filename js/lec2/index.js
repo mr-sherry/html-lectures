2205833e8902b1f4f4b8f6095bfed675e088e12d
mr.sherry <mr.sherry6967@gmail.com>
2025-11-25 16:29:59 +0500

    string methods and operators

---
+++ js/lec2/index.js
@@ -0,0 +1,25 @@
+let num = 8238;
+let num2 = 922;
+console.log(num + num2);
+
+// console.log(user && logedin);
+// console.log(user || logedin);
+
+let str = 'Aahmed raza shahid';
+let str2 = '      raza    ';
+
+console.log(str.length);
+console.log(str.charAt(3));
+console.log(str.charCodeAt(0));
+console.log(str.codePointAt(1));
+console.log(str.length);
+console.log(str.concat(str2));
+console.log(str.at(0));
+
+console.log(str.slice(0, 3));
+console.log(str.substr(0, 2));
+
+console.log(str2.trim());
+console.log(str.replace('a', 'm'));
+
+console.log(str.split(' '));
