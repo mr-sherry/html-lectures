bfcdf68ba7be0f206b50f99e2f3893d006a777d4
mr.sherry <mr.sherry6967@gmail.com>
2025-12-05 16:48:45 +0500

    date and math

---
+++ js/lec5/index.js
@@ -0,0 +1,25 @@
+// let day = 24 * 60 * 60 * 1000;
+let date = new Date().toLocaleString('ar-SA');
+let date2 = new Date('July 21, 1983 01:15:00.250');
+
+let milli = new Date().getMilliseconds();
+
+console.log();
+
+// let user = { dasjlaksd };
+
+// save(user + Date().now);
+
+let num = 123;
+
+console.log(Math.random() * 10);
+
+// if (condition) {
+// } else {
+// }
+
+let data = false;
+
+data ? console.log('data comming') : console.log('data not comming');
+ 
+// (condition ) ? (code)
