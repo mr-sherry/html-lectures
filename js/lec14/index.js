af82aa7817ce67f28c862b5010b52267bcb92e75
mr.sherry <mr.sherry6967@gmail.com>
2025-12-22 16:48:12 +0500

    clousers and event loop

---
+++ js/lec14/index.js
@@ -0,0 +1,63 @@
+// function name() {
+//   let number = 0;
+
+//   console.log(number + 1);
+// }
+
+// // name();
+// // name();
+// // name();
+
+// function counter() {
+//   let number = 0;
+//   function childCount() {
+//     number = number + 1;
+//   }
+
+//   childCount();
+
+//   return number;
+// }
+
+// let x = 0;
+// x = counter();
+// x = counter();
+book;
+let inp = document.getElementById('myinput');
+let input = 'ahmed';
+
+inp.addEventListener('input', function name() {
+  input = inp.value;
+});
+
+
+setTimeout(() => {
+    
+    api
+}, 2000);
+
+
+
+
+let result = 'book';
+
+let books = {};
+books.input;
+
+
+
+// x = counter();
+// console.log(x);
+
+// // func();
+// // func();
+// // func();
+
+// event loop
+
+setTimeout(() => {
+  console.log('timer');
+}, 0);
+
+console.log('hello last');
+console.log('hello');
