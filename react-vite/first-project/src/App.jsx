9b3c8d6a0d667fc904da5eaf65fd03fc20585007
mr.sherry <mr.sherry6967@gmail.com>
2025-12-26 17:05:54 +0500

    react with vite

---
+++ react-vite/first-project/src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <>
+      <h1>hello</h1>
+      <div>npm create vite@latest project-name</div>
+      <div>cd project-name</div>
+      <div>npm install</div>
+      <div>npm run dev</div>
+    </>
+  );
+}
+
+export default App;
